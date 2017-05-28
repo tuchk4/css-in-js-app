5cd01587e05c3d8cfcd4dc0f8129daa9e7c6e66f
lttb <kenzhaev.artur@gmail.com>
2017-05-28 21:55:02 +0300

    Fix DifferentBlocks for styled-jss

--- src/scenes/StyledJss/DifferentBlocks.js
+++ src/scenes/StyledJss/DifferentBlocks.js
@@ -7,7 +7,7 @@ const components = [];
 for (let i = 0; i < config.size; i++) {
   let size = Math.round(i / 10 % 1 * 10);
 
-  const component = styled(Block, {
+  const component = styled(Block)({
     border: `${size + 2}px solid #000`,
     borderRadius: `${size * 6}px`,
   });
