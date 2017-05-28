daecd455835de5e1f2e99c2bc7f4a560f611b2c7
Valeriy Sorokobatko <valeriy.sorokobatko@gmail.com>
2017-05-28 18:05:13 +0300

    add hover for inline styles

--- src/scenes/ReactInline/Block.js
+++ src/scenes/ReactInline/Block.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import colors from '../../utils/colors';
 
-export default props => {
-  return (
-    <div
-      style={{
-        minWidth: '16px',
-        height: '16px',
-        display: 'inline-block',
-        textAlign: 'center',
-        padding: '15px',
-        border: 'none',
-        fontWeight: 'bold',
-        backgroundColor: props.isPrimary
-          ? colors[props.i][1]
-          : colors[props.i][0],
-        ...props.style,
-      }}
-    >
-      {props.children}
-    </div>
-  );
-};
+export default class Block extends React.Component {
+  state = {
+    hoverStyles: {},
+  };
+
+  onMouseOver = () => {
+    this.setState({
+      hoverStyles: {
+        backgroundColor: 'black',
+        color: 'white',
+      },
+    });
+  };
+
+  onMouseOut = () => {
+    this.setState({
+      hoverStyles: {},
+    });
+  };
+
+  render() {
+    const props = this.props;
+
+    return (
+      <div
+        onMouseOver={this.onMouseOver}
+        onMouseOut={this.onMouseOut}
+        style={{
+          minWidth: '16px',
+          height: '16px',
+          display: 'inline-block',
+          textAlign: 'center',
+          padding: '15px',
+          border: 'none',
+          fontWeight: 'bold',
+          backgroundColor: props.isPrimary
+            ? colors[props.i][1]
+            : colors[props.i][0],
+          ...props.style,
+          ...this.state.hoverStyles,
+        }}
+      >
+        {props.children}
+      </div>
+    );
+  }
+}
